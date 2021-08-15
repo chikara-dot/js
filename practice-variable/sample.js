2c18a42eb88e2c88039ddf25e07352e3861810ee
勝城睦楽 <victorycastlepower@gmail.com>
2021-08-15 18:26:20 +0900

    8/15

---
+++ practice-variable/sample.js
@@ -0,0 +1,13 @@
+let nickname = "taro"
+console.log(nickname)
+nickname = "ichiro"
+console.log(nickname)
+
+var str = "webcamp"
+function foo(){
+  console.log(str)
+  var str = "dmm webcamp"
+  console.log(str)
+}
+
+foo()
